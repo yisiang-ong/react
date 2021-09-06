5de96192f15ff284b0d09646a0a032c9e5a77868
Yisiang <ongyisiang98@yahoo.com>
2021-09-06 23:18:31 +0800

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle,Breadcrumb,BreadcrumbItem,Button,Modal,ModalHeader,ModalBody,Col,Row,Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {Control,LocalForm,Errors} from 'react-redux-form';
-
+import { Loading } from './LoadingComponent';
     function RenderComments({comments, addComment, dishId}) {
         const dateformatter = new Intl.DateTimeFormat("en-US", { 
             year: "numeric",
@@ -155,7 +155,25 @@ import {Control,LocalForm,Errors} from 'react-redux-form';
         }
     const DishDetail = (props) => {
         
-        if (props.dish != null)
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null)
             return(
                 <div className="container">
                     <div className="row">
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Card, CardImg, CardImgOverlay,
     CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 
     function RenderMenuItem ({dish, onClick}) {
         return (
@@ -21,7 +21,7 @@ import { Link } from 'react-router-dom';
     
 
     const Menu = (props) => {
-        const menu = props.dishes.map((dish) => {
+        const menu = props.dishes.dishes.map((dish) => {
             return (
                 <div className="col-12 col-md-5 m-1" key={dish.id}>
                     <RenderMenuItem dish={dish} onClick={props.onClick}/>
@@ -29,24 +29,44 @@ import { Link } from 'react-router-dom';
             );
         });
 
-
-        return (
-          <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home"></Link>Home</BreadcrumbItem>
-                    <BreadcrumbItem active>Menu</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Menu</h3>
-                    <hr />
+        if (props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row"> 
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else
+            return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home"></Link>Home</BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    {menu}
                 </div>
             </div>
-            <div className="row">
-                {menu}
-            </div>
-          </div>
-        );
+            );
     }
 
 
