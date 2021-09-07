15768ee1b3fa63c2065b5bc7469d71e2cfbb3ae0
Yisiang <ongyisiang98@yahoo.com>
2021-09-07 21:14:27 +0800

    React Animation Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
     function RenderComments({comments, postComment, dishId}) {
         const dateformatter = new Intl.DateTimeFormat("en-US", { 
             year: "numeric",
@@ -17,14 +18,18 @@ import { baseUrl } from '../shared/baseUrl';
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        {comments.map((comment) => {
-                            return(
-                                <li key={comment.id}>
-                                    <p>{comment.comment}</p>
-                                    <p>--{comment.author}, {dateformatter.format(new Date(Date.parse(comment.date)))}</p>
-                                </li>
-                            ); 
-                        })}
+                        <Stagger in>
+                            {comments.map((comment) => {
+                                return(
+                                    <Fade in>
+                                    <li key={comment.id}>
+                                        <p>{comment.comment}</p>
+                                        <p>--{comment.author}, {dateformatter.format(new Date(Date.parse(comment.date)))}</p>
+                                    </li>
+                                    </Fade>
+                                ); 
+                            })}
+                        </Stagger>
                     </ul>
                     {/* Render CommentForm from CommentForm.js */}
                     <CommentForm dishId={dishId} postComment={postComment} />
@@ -40,13 +45,19 @@ import { baseUrl } from '../shared/baseUrl';
         if (dish != null) {
           return (  
             <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-                    <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                <FadeTransform
+                    in
+                    transformProps={{
+                        exitTransform: 'scale(0.5) translateY(-50%)'
+                    }}>
+                    <Card>
+                        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+                        <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </FadeTransform>
             </div>
           );
         } else {
